c846a151424d75dd10aba0b42617e06cf867edbe
Vishwanath <vishwanathshankar0703@gmail.com>
2019-08-10 14:12:24 +0530

    First commit of the standings microservice

---
+++ tests/apiTests.js
@@ -0,0 +1,11 @@
+// Test case for wrong team name
+// Right 
+
+// Test case for wrong team name
+// Right
+
+// Test case for wrong country name
+// Right
+
+// Test case for wrong league name 
+// Right
